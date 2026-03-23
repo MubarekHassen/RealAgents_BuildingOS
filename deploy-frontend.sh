4378e19767300ca439aef989264ba0066f4fbcd0
Ghundar457 <ayman.abumaike1@gmail.com>
2026-03-23 14:00:42 -0400

    chore: Remove all debug/check/test scripts from root

--- deploy-frontend.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Deploy frontend to Vercel (buildinginitial project)
-# Usage: ./deploy-frontend.sh
-
-echo "Deploying frontend to Vercel (buildinginitial)..."
-cd "$(dirname "$0")"
-npx vercel --prod --yes
-echo ""
-echo "Done! Visit https://buildinginitial.vercel.app to verify."
